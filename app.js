7a0567b04557bff943e6a9f7ed9797ef1b38be75
03332387456 <sahirmavia@gmail.com>
2023-05-29 15:11:53 +0500

    java assignment 8

---
+++ app.js
@@ -0,0 +1,15 @@
+function transformToCircle() {
+  var shape = document.getElementById("shape");
+  shape.className = "shape circle";
+}
+
+function transformTosquare() {
+  var shape = document.getElementById("shape");
+  shape.className = "shape square";
+}
+
+function transformToTriangle() {
+  var shape = document.getElementById("shape");
+  shape.className = "shape triangle";
+}
+
